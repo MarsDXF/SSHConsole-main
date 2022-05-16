59737fb4ce506e60c1caa9f4790036ece9cbf5cc
Vasiliy <Freeneos@icloud.com>
2022-05-16 19:13:40 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,97 @@
+# SSHConsole
+
+A simple command console for servers using [SwiftNIO SSH]("https://github.com/apple/swift-nio-ssh").
+
+## Why Would I Use This?
+
+This package provides an SSH listener to your server which allows you to `ssh` commmands to your server 
+and receive the response back to your terminal.
+
+You might use this to report on your server's internals, issue tuning commands, or tell it to restart or shutdown.
+
+It is MIT licensed. Go wild.
+
+## How Much Work is This?
+
+Just **three** lines of code.
+
+Plus 5 to manage your *host key* so you don't annoy yourself by having to approve a new one 
+each time you restart your server.
+
+Plus 10 more to have some sort of responsible authentication.
+
+Plus however you do your commands. I use [Swift Argument Parser](https://github.com/apple/swift-argument-parser) to define 
+my commands, which is great, but not quite intended to be used this way and there is a page of extensions required 
+to work around it's limitations. But strictly speaking, that's not part of this project and you can do whatever you 
+want with your commands.
+
+## Can You Show Me?
+
+```swift
+import SSHConsole
+
+/// get your program doing it's stuff
+
+let console = SSHConsole( port:2525, 
+                          hostKeys:hostKeys, 
+                          passwordDelegate: TrivialPassword(), 
+                          publicKeyDelegate: AuthenticatePublicKey() )
+try console.listen( runner: myHandler )
+
+func myHandler( command: String, to: SSHConsole.CommandHandler.Output, user:String, environment: [String : String]) {
+    to.write("Thank you for connecting to my demo service.\r\n")
+    to.write("Good bye now.\r\n")
+}
+
+/// wait around in your program until you are ready to exit
+
+try console.stop()   // Or don't, but this flushes and closes the open sockets
+```
+
+Ok, I've lied there. You will notice that `TrivialPassword()` seems like a really bad idea, `AuthenticatePublicKey()`
+is not defined. But go look in the example Echo server. The
+whole think *with* all the missing bits as ~130 lines and that's mostly comments for you.
+
+Here is the [complete source for the Echo server](https://github.com/jimstudt/SSHConsole/blob/main/Sources/Echo/main.swift)
+
+## How Much Does This Drag In?
+
+- [SwiftNIO SSH](https://github.com/apple/swift-nio-ssh)
+
+- SwiftNIO SSH will then drag in [SwiftNIO](https://github.com/apple/swift-nio/), and 
+  [Swift Crypto](https://github.com/apple/swift-crypto) but you 
+  probably already have them if you are writing a server.
+
+## It Looks Small, What Kind of Limitiations Does It Have?
+
+- It only does commands with no input. If you try to send input it will chastise you and destroy your 
+  sockets. I thought about adding sessions, but that is more code than the whole thing now, 
+  and if I give you that you'll want command line editting and recall, and well, it all can get out of hand 
+  in a hurry. There is a utility which lets you edit commands locally and ship them off on enter. 
+  But I've forgotten the name. Still, it would be great with this. Maybe someone will remind me and
+  I'll suggest it here.
+
+- It doesn't handle RSA keys. This is a, possibly permanent, limitation of the SwiftNIO SSH project.
+
+- It only does ED25519 host keys. This can be expanded, but I'm annoyed at Swift NIO blinding 
+  me to the private keys and I'm hoping they'll open that up and I won't have to write the 20 lines of
+  code to expand my shadow private key logic I use to work around the blinding.
+
+## How Stable is the API?
+
+Mostly stable. There is still a problem where I can't find the authenticated user name, so I'm 
+passing "???". That will either get resolved by me finding the name, or removing the ability
+to know which user is running your command.
+
+## How Committed Is The Maintainer?
+
+Not at all. This is a two-day rathole I ran down for my own servers. I'll use it and probably spend
+another day polishing it, but if you are a sizeable organization or an active developer and want to use it, 
+maybe I should give it to you. (I also vanish for months at a time, so responses are not timely.) – *fork is your friend*
+
+## What's Next?
+
+- Still working on retrieving the authenticated user, sending "???" for now.
+  
+- Can I send an exit code back through this to the client?
+
---
+++ Sources/SSHConsole/CommandHandler.swift
@@ -0,0 +1,186 @@
+//
+//  CommandHandler.swift
+//  
+//  Created by Jim Studt on 11/20/20.
+//
+//  SPDX-License-Identifier: MIT
+//
+
+import Dispatch
+import Foundation
+import NIO
+import NIOSSH
+
+extension SSHConsole {
+    
+    /// The command handler for an SSHConsole connection.
+    ///
+    public class CommandHandler: ChannelDuplexHandler {
+        public typealias InboundIn = SSHChannelData
+        public typealias InboundOut = ByteBuffer
+        public typealias OutboundIn = ByteBuffer
+        public typealias OutboundOut = SSHChannelData
+        
+        public let runner : Runner
+        public let user : String
+        
+        private let queue = DispatchQueue(label: "ssh sync")
+        private var environment: [String: String] = [:]
+        
+        private var madeStream : Bool = false
+        private weak var _outputStream : Output? = nil
+        
+        public required init(runner: @escaping Runner, user:String) {
+            self.runner = runner
+            self.user = user
+        }
+        
+        //
+        // This is a bit odd. You can take a bunch of these, when the last one
+        // goes, then we get closed. In practice I think people will only make
+        // one, but this semantic lets me avoid communicating an error if they
+        // call more than once.
+        //
+        internal var outputStream : Output {
+            if let stream = _outputStream { return stream }
+            
+            if madeStream { fatalError("remaking an outputStream")}
+            madeStream = true
+            
+            let s = Output(handler: self)
+            _outputStream = s
+            return s
+        }
+        
+        weak var context : ChannelHandlerContext? = nil
+        
+        //
+        // Always turn on our .allowRemoteHalfClosure because SSH needs it.
+        //
+        public func handlerAdded(context: ChannelHandlerContext) {
+            self.context = context        // save it for writing later
+            context.channel.setOption(ChannelOptions.allowRemoteHalfClosure, value: true).whenFailure { error in
+                context.fireErrorCaught(error)
+            }
+        }
+        
+        public func userInboundEventTriggered(context: ChannelHandlerContext, event: Any) {
+            switch event {
+            case let event as SSHChannelRequestEvent.ExecRequest:
+                runner( event.command, outputStream, user, environment)
+                
+            case let event as SSHChannelRequestEvent.EnvironmentRequest:
+                self.queue.sync {
+                    environment[event.name] = event.value
+                }
+                
+            default:
+                context.fireUserInboundEventTriggered(event)
+            }
+        }
+        
+        public func channelRead(context: ChannelHandlerContext, data: NIOAny) {
+            let data = self.unwrapInboundIn(data)
+            
+            /*
+             guard case .byteBuffer(let bytes) = data.data else {
+             fatalError("Unexpected read type")
+             }
+             */
+            guard case .channel = data.type else {
+                context.fireErrorCaught(SSHConsole.ProtocolError.invalidDataType)
+                return
+            }
+            
+            let buf = context.channel.allocator.buffer(string: "Input Not Accepted\n\r")
+            
+            let prom = context.eventLoop.makePromise(of:Void.self)
+            prom.futureResult.whenComplete( { _ in _ = context.close() })
+            
+            writeError(context:context, data:NIOAny(buf), promise: prom)
+            //context.fireChannelRead(self.wrapInboundOut(bytes))
+        }
+        
+        public func write(context: ChannelHandlerContext, data: NIOAny, promise: EventLoopPromise<Void>?) {
+            let data = self.unwrapOutboundIn(data)
+            context.writeAndFlush(self.wrapOutboundOut(SSHChannelData(type: .channel, data: .byteBuffer(data))), promise: promise)
+        }
+        
+        public func writeError(context: ChannelHandlerContext, data: NIOAny, promise: EventLoopPromise<Void>?) {
+            let data = self.unwrapOutboundIn(data)
+            
+            context.writeAndFlush(self.wrapOutboundOut( SSHChannelData(type: .stdErr, data: .byteBuffer(data))), promise: promise)
+        }
+        
+    }
+}
+
+extension SSHConsole.CommandHandler {
+    public func write(text: String, promise: EventLoopPromise<Void>?) {
+        guard let c = context else { return }
+        
+        c.eventLoop.execute {
+            let buf = c.channel.allocator.buffer(string: text)
+
+            self.write(context: c, data: NIOAny(buf), promise: promise)
+        }
+    }
+
+    public func writeError(text: String, promise: EventLoopPromise<Void>?) {
+        guard let c = context else { return }
+        
+        c.eventLoop.execute {
+            let buf = c.channel.allocator.buffer(string: text)
+
+            self.writeError(context: c, data: NIOAny(buf), promise: promise)
+        }
+    }
+
+   //
+    // Flush and close
+    //
+    public func close( promise: EventLoopPromise<Void>?) {
+        guard let c = context else { return }
+        
+        c.eventLoop.execute {
+            c.flush()
+            c.close(promise: promise)  // does this need to wait for the flush and write?
+        }
+    }
+    
+    /// An output stream for sending data back to the SSH client.
+    public class Output : TextOutputStream {
+        private let handler : SSHConsole.CommandHandler
+        
+        internal init( handler: SSHConsole.CommandHandler ) {
+            self.handler = handler
+        }
+        deinit {
+            handler.close(promise: nil)
+        }
+        /// Send data back to the client
+        ///
+        /// You will probably want to end lines with "\r\n" to make terminal clients do the right thing.
+        ///
+        /// This output may be buffered arbitrarily long until you finish your command and release
+        /// the Output object.
+        ///
+        /// - Parameter string: Text to send
+        public func write(_ string: String) {
+            handler.write(text:string, promise: nil)
+        }
+        
+        /// Send data back to the client on its standard error stream
+        ///
+        /// You will probably want to end lines with "\r\n" to make terminal clients do the right thing.
+        ///
+        /// This output may be buffered arbitrarily long until you finish your command and release
+        /// the Output object.
+        ///
+        /// - Parameter string: Text to send
+        public func writeError(_ string: String) {
+            handler.writeError(text:string, promise: nil)
+        }
+   }
+
+}
